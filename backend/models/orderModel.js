03984508d08bc3c34366edf1ef4bfa6e28cbe0c5
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-20 08:18:59 +0530

    E-C-W

---
+++ backend/models/orderModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    products: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+    }],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Processing',
+    },
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
