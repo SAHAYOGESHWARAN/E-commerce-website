46f7ab079489c6f7bbe20945a8a759af4f4672c6
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-20 08:32:18 +0530

    E-C-W

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -17,8 +17,16 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-}, { timestamps: true });
+    category: {
+        type: String,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
 
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
